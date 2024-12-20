d223bcb8121fc047cc69a2954a9a6382ebc19dfb
Jason Simeone <jay@classless.net>
2024-12-20 11:55:56 -0500

    Day 2024-09 part 2

--- 2024/day9/src/main.rs
+++ 2024/day9/src/main.rs
@@ -2,10 +2,17 @@ use std::env;
 use std::fs;
 use std::io;
 use std::io::BufRead;
-use std::ops::Mul;
+use std::usize;
 
 
 type Disk = Vec<i32>;
+
+#[derive(Debug)]
+struct Section {
+    location: usize,
+    size: usize,
+    id: i32,
+}
 
 const EMPTY: i32 = -1;
 
@@ -66,6 +73,94 @@ fn defrag(disk: &Disk) -> Disk {
     defragged
 }
 
+fn index_disk(disk: &Disk) -> (Vec<Section>, Vec<Section>) {
+    let mut filled: Vec<Section> = Vec::new();
+    let mut empty: Vec<Section> = Vec::new();
+
+    let mut last_start: usize = usize::MAX;
+    let mut last_value: i32 = -999;
+    for (idx, value) in disk.iter().enumerate() {
+        if *value != last_value {
+            if last_start != usize::MAX {
+                let section = Section {
+                    location: last_start,
+                    size: idx - last_start,
+                    id: last_value,
+                };
+
+                if last_value == EMPTY {
+                    empty.push(section);
+                } else {
+                    filled.push(section);
+                }
+            }
+            last_start = idx;
+            last_value = *value;
+        }
+    }
+
+    let section = Section {
+        location: last_start,
+        size: disk.len() - last_start,
+        id: last_value,
+    };
+    if last_value == EMPTY {
+        empty.push(section);
+    } else {
+        filled.push(section);
+    }
+
+    (filled, empty)
+}
+
+fn defrag_full(disk: &Disk) -> Disk {
+    let mut defragged: Disk = disk.clone();
+    let (filled, mut empty) = index_disk(disk);
+
+    for fblock in filled.iter().rev() {
+        for (idx, eblock) in empty.iter().enumerate() {
+            if fblock.size > eblock.size { continue; }
+            if fblock.location < eblock.location { break; }
+
+            let size_diff = eblock.size - fblock.size;
+
+            if size_diff == 0 {
+                for x in eblock.location..(eblock.location + eblock.size) {
+                    defragged[x] = fblock.id;
+                }
+                for x in fblock.location..(fblock.location + fblock.size) {
+                    defragged[x] = EMPTY;
+                }
+
+                empty.remove(idx);
+                break;
+            } else if size_diff > 0 {
+                for x in eblock.location..(eblock.location + fblock.size) {
+                    defragged[x] = fblock.id;
+                }
+                for x in fblock.location..(fblock.location + fblock.size) {
+                    defragged[x] = EMPTY;
+                }
+
+                empty.insert(idx, Section {
+                    location: eblock.location + fblock.size,
+                    size: eblock.size - fblock.size,
+                    id: eblock.id,
+                });
+                empty.remove(idx + 1);
+                break;
+            }
+        }
+    }
+
+    defragged
+}
+
+fn print_disk(disk: &Disk) {
+    let strs: Vec<String> = disk.iter().map(|&v| if v >= 0 { v.to_string() } else { String::from(".") }).collect();
+    println!("{}", strs.join(""));
+}
+
 fn checksum(disk: &Disk) -> i64 {
     let mut sum: i64 = 0;
 
@@ -84,6 +179,10 @@ fn main() {
     let input_filename = args.get(1).expect("must specify input file");
 
     let disk = parse_file(input_filename);
+
     let defragged = defrag(&disk);
     println!("Part 1 Checksum: {}", checksum(&defragged));
+
+    let defragged = defrag_full(&disk);
+    println!("Part 2 Checksum: {}", checksum(&defragged));
 }
